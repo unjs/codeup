d495314320470e378cc5df28579527978b7c1682
Pooya Parsa <pooya@pi0.io>
2026-01-29 10:46:33 +0100

    update

--- build.config.ts
+++ build.config.ts
@@ -2,12 +2,4 @@ import { defineBuildConfig } from "obuild/config";
 
 export default defineBuildConfig({
   entries: ["src/index.ts", "src/cli/index.ts", "src/utils/index.ts"],
-
-  hooks: {
-    rolldownOutput(cfg) {
-      cfg.chunkFileNames = () => {
-        return "_[name].mjs";
-      };
-    },
-  },
 });
